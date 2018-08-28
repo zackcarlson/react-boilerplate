1fe6a457f511c9f375162fabea345590aff75157
zackcarlson <zackt.carlson@gmail.com>
2018-08-27 18:58:25 -0700

    Import path module and add wildcard for all routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const public = '/../client/dist/';
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + public));
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + public + 'index.html'));
 });
 
 app.listen(PORT, () => {
