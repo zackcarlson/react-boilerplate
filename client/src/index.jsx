a02bf6ba663ec57788e6476853dfde2710b0992e
zackcarlson <zackt.carlson@gmail.com>
2018-08-27 18:57:04 -0700

    Add browserHistory to preserve screen reload

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import '../dist/style.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter history={browserHistory}>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
